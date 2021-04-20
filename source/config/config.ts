04487d6231d26bba3732828d83c54dc835263962
BigB97 <bigbofficial01@gmail.com>
2021-04-20 23:28:59 +0100

    news

--- source/config/config.ts
+++ source/config/config.ts
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
-const SERVER_PORT = process.env.SERVER_PORT || 3000;
+const SERVER_PORT = process.env.SERVER_PORT || 1337;
 
 const SERVER = {
     hostname: SERVER_HOSTNAME,
