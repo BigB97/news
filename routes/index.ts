5e3be30d12ec1903d9fd08adf6548d4c691e956a
BigB97 <bigbofficial01@gmail.com>
2021-04-21 14:05:49 +0100

    foodCrowdy

--- routes/index.ts
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* POST news listing. */
-router.post('/', (req, res) => {
-  const {title,} = data;
-});
-
-module.exports = router;
--- routes/users.ts
+++
@@ -1,9 +0,0 @@
-// const express = require('express');
-const router = express.Router();
-
-/* POST news listing. */
-router.post('/', (req, res) => {
- 
-});
-
-module.exports = router;
